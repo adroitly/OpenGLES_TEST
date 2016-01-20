bac65ffd753714194c665d3c8224f47818bf918c
adroitly <adroitly@163.com>
2016-01-20 20:45:25 +0800

    Signed-off-by: adroitly <adroitly@163.com>

---
+++ UseObj/src/com/killer/useobj/MainActivity.java
@@ -0,0 +1,36 @@
+package com.killer.useobj;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+	private MySurfaceView mySufaceView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置为全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE); 
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,  
+		              WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		//设置为横屏模式
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		mySufaceView = new MySurfaceView(this);
+		setContentView(mySufaceView);
+		mySufaceView.requestFocus();//获取焦点
+		mySufaceView.setFocusableInTouchMode(true);//设置为可触控  
+		
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
